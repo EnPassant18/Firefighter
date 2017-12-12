606a6e20dfe351a1ac742228773cf7fe8a01dfe1
Daniel <daniel_kostovetsky@brown.edu>
2017-12-12 12:15:26 -0500

    Fixed improper style in startup.js

--- startup.js
+++ startup.js
@@ -1,8 +1,8 @@
-REFRESH_RATE = 100
-SPAWN_AREA_HEIGHT = 10
-WORLD_HEIGHT = 80
-WORLD_WIDTH = 120
-CELL_SIZE = 10
+let REFRESH_RATE = 100
+const SPAWN_AREA_HEIGHT = 10
+const WORLD_HEIGHT = 80
+const WORLD_WIDTH = 120
+const CELL_SIZE = 10
 const startState = array2d(SPAWN_AREA_HEIGHT + WORLD_HEIGHT, WORLD_WIDTH, () => false);
 world = new World(startState);
 world.init();
