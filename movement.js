8b4b16fc343ca832d1aae6a1e78cb7c0d5f4ee71
Daniel <daniel_kostovetsky@brown.edu>
2017-12-13 15:53:55 -0500

    Reworked movement and shoorting mechanism

--- movement.js
+++ movement.js
@@ -6,14 +6,18 @@ const SPACE_KEY_CODE = 32
 
 document.onkeydown = function(event) {
     if (event.keyCode == LEFT_KEY_CODE) { 
-        moveDirection = "left";
+        if (moveDirection != "left") {
+            moveDirection = "left";
+            move("left");
+        }
     } else if (event.keyCode == RIGHT_KEY_CODE) { 
-        moveDirection = "right";
-    } else if (event.keyCode == SPACE_KEY_CODE) {
+        if (moveDirection != "right") {
+            moveDirection = "right";
+            move("right");
+        }
+    } /*else if (event.keyCode == SPACE_KEY_CODE) {
         world.shoot(Math.round(parseInt(document.getElementById("player").style.left) / CELL_SIZE + 1));
-    } else if (event.shiftKey == true) {
-        world.clearBlue()
-    }
+    }*/
 }
 
 document.onkeyup = function(event) {
@@ -22,35 +26,19 @@ document.onkeyup = function(event) {
     }
 }
 
-/* Direction input is a velocity vector. Moves player 5 times 
-by its magnitude in its direction. */
+// Moves player by one cell in given direction.
 function move(direction) {
-    let i = 0
-    let interval = setInterval(function() {
-        const newPosition = parseInt(document.getElementById("player").style.left) + direction
-        if (newPosition < 0) {
-            document.getElementById("player").style.left = "0px";
-        } else if (newPosition >= MAX_POSITION) {
-            document.getElementById("player").style.left = MAX_POSITION + "px";
-        } else {
-            document.getElementById("player").style.left = newPosition + "px";
-        }
-        if (++i == 10) clearInterval(interval);
-    }, 1)
+    const currentPosition = parseInt(document.getElementById("player").style.left);
+    if (direction == "left" && currentPosition > 0 && moveDirection == "left") {
+        document.getElementById("player").style.left = currentPosition - 15 + "px";
+        setTimeout(move, 50, direction);
+    } else if (direction == "right" && currentPosition < MAX_POSITION && moveDirection == "right") {
+        document.getElementById("player").style.left = currentPosition + 15 + "px";
+        setTimeout(move, 50, direction);
+    }
 }
 
 setInterval(function() {
-    if (moveDirection == "left") {
-        move(-3)
-    } else if (moveDirection == "right") {
-        move(3)
-    } 
-}, 50)
-
-World.prototype.clearBlue = function() {
-    for (let row = 0; row < this.height; row++) {
-        for (let col = 0; col < this.width; col++) {
-            if (this.grid[row][col] == "blue") { this.grid[row][col] = "dead" }
-        }
-    }
-}
+    world.shoot.call(world, parseInt(document.getElementById("player").style.left) / CELL_SIZE + 1)
+}, 500)
+    
